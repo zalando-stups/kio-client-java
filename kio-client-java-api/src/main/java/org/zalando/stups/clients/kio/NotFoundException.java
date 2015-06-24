7c44140659bf72892230acc837039a352beb54fa
jbellmann <joerg.bellmann@zalando.de>
2015-06-24 17:04:43 +0200

    fix typo

--- kio-client-java-api/src/main/java/org/zalando/stups/clients/kio/NotFoundException.java
+++ kio-client-java-api/src/main/java/org/zalando/stups/clients/kio/NotFoundException.java
@@ -18,7 +18,7 @@ package org.zalando.stups.clients.kio;
 /**
  * @author  jbellmann
  */
-public class NotFoudException extends RuntimeException {
+public class NotFoundException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
 
